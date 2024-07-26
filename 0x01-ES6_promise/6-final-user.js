a9677db06d3d32c44907ec773903aad2980c5afe
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-07-26 22:54:02 +0100

    fix linter

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,13 @@
-#!/usr/bin/env node
-
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+const handleProfileSignup = (firstName, lastName, fileName) => {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
   return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => results.map((obj) => ({
-      status: obj.status,
-      value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
     })));
-}
+};
+export default handleProfileSignup;
